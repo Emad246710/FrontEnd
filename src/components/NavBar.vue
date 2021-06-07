8ab0b4b9d9535c976a20c49d47cee99a66126c41
ahaggi <a.haggi@yahoo.com>
2021-06-07 22:14:56 +0200

    sidebar fix

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,111 +2,85 @@
   <nav
     id="sidebarMenu"
     class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
+    ref="sidebarMenuRef"
   >
+    <!-- 
+      <button
+        data-bs-toggle="collapse"
+        data-bs-target="#sidebarMenu"
+        aria-controls="sidebarMenu"
+        aria-expanded="false"
+      >
+        Hide the bar   
+      </button> 
+      <a
+        href="#sidebarMenu"
+        data-bs-toggle="collapse"
+        aria-expanded="false"
+        aria-controls="sidebarMenu" 
+      >
+        Hide the bar   
+      </a>
+
+    -->
     <div class="position-sticky pt-3">
       <ul class="nav flex-column">
-        <li class="nav-item">
-          <router-link :to="{ name: 'notes' }" class="nav-link">
-            <span data-feather="home"></span>
-            Notes</router-link
-          >
-        </li>
-        <li class="nav-item">
-          <router-link :to="{ name: 'categories' }" class="nav-link">
-            <span data-feather="file"></span>
-            categories
+        <li class="nav-item" @click="hideSidebarWhenScreenLTmd">
+          <router-link :to="{ name: 'notes' }" v-slot="{ navigate, isActive }">
+            <a
+              class="nav-link"
+              :class="{
+                'bg-primary text-light': isActive,
+              }"
+              @click="navigate"
+              href="whatEver!"
+              >Notes</a
+            >
           </router-link>
         </li>
 
-        <li class="nav-item">
-          
-          <router-link class="nav-link" :to="{ name: 'note_create' }">
-            <span data-feather="users"></span>
-            note_create
+        <li class="nav-item" @click="hideSidebarWhenScreenLTmd">
+          <router-link :to="{ name: 'categories' }" v-slot="{ isActive }">
+            <span
+              class="nav-link"
+              :class="{
+                'bg-primary text-light': isActive,
+              }"
+              >Categories</span
+            >
           </router-link>
         </li>
-
-        <li class="nav-item">
-          
-
-          <router-link class="nav-link" :to="{ name: 'category_create' }">
-            <span data-feather="users"></span>
-            category_create
+        <li class="nav-item" @click="hideSidebarWhenScreenLTmd">
+          <router-link :to="{ name: 'note_create' }" v-slot="{ isActive }">
+            <span
+              class="nav-link"
+              :class="{
+                'bg-primary text-light': isActive,
+              }"
+              >note_create</span
+            >
           </router-link>
         </li>
-
-
-        <li class="nav-item">
-          
-          <router-link class="nav-link" :to="{ name: 'login' }">
-            <span data-feather="layers"></span>
-            login
+        <li class="nav-item" @click="hideSidebarWhenScreenLTmd">
+          <router-link :to="{ name: 'category_create' }" v-slot="{ isActive }">
+            <span
+              class="nav-link"
+              :class="{
+                'bg-primary text-light': isActive,
+              }"
+              >category_create</span
+            >
           </router-link>
         </li>
-        
-
-        <li class="nav-item">
-         
-          <router-link class="nav-link" :to="{ name: 'signup' }">
-            <span data-feather="file-text"></span>
-            signup
-          </router-link>
-        </li>
-        <li class="nav-item">
-          
-          <router-link
-            class="nav-link"
-            :to="{ name: 'note', params: { id: 1 } }"
-          >
-            <span data-feather="file-text"></span>
-
-            note
-          </router-link>
-        </li>
-
-        <li class="nav-item">
-          
-          <router-link
-            class="nav-link"
-            :to="{ name: 'category_edit', params: { id: 1 } }"
-          >
-            <span data-feather="file-text"></span>
-
-            category_edit
-          </router-link>
-        </li>
-
-        <li class="nav-item">
-          
-          <router-link
-            class="nav-link"
-            :to="{ name: 'user_edit', params: { id: 1 } }"
-          >
-            <span data-feather="file-text"></span>
-
-            user_edit
-          </router-link>
-        </li>
-
-        <li class="nav-item">
-          
-          <router-link
-            class="nav-link"
-            :to="{ name: 'note_edit', params: { id: 1 } }"
-          >
-            <span data-feather="file-text"></span>
-            note_edit
-          </router-link>
-        </li>
-
-        <li class="nav-item">
-          
-          <router-link
-            class="nav-link"
-            :to="{ name: 'user' }"
-          >
-            <span data-feather="file-text"></span>
-            user
+        <li class="nav-item" @click="hideSidebarWhenScreenLTmd">
+          <router-link :to="{ name: 'user' }" v-slot="{ isActive }">
+            <span
+              class="nav-link"
+              :class="{
+                'bg-primary text-light': isActive,
+              }"
+              >User</span
+            >
           </router-link>
         </li>
       </ul>
@@ -115,7 +89,32 @@
 </template>
 
 <script>
-export default {};
+import { Collapse } from "bootstrap";
+import { ref, onMounted } from "vue";
+
+export default {
+  setup(props, context) {
+    // https://markus.oberlehner.net/blog/refs-and-the-vue-3-composition-api/
+    const sidebarMenuRef = ref(null);
+
+    const hideSidebarWhenScreenLTmd = () => {
+      // Since the cssClass "show" will be set ONLY when ( (the viewPort < md) && (the sidebar is visible) )
+      // We can hide the sidebar (which fills the screen), when the user clicks on a link by removing cssClass "show"
+      // This means this function will have no effect when the sidebar does not fill the screen, by other
+      // words: when the viewPort larger than md
+      console.log(sidebarMenuRef.value.classList);
+      // https://developer.mozilla.org/fr/docs/Web/API/Element/classList
+      sidebarMenuRef.value.classList.remove("show");
+      console.log(sidebarMenuRef.value.classList);
+    };
+
+    onMounted(() => {
+      console.log(sidebarMenuRef.value.classList);
+    });
+
+    return { sidebarMenuRef, hideSidebarWhenScreenLTmd };
+  },
+};
 </script>
 
 <style>
